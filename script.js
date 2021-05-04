8516ed41903b8c5deb36cc95379122599811a61c
lpsorg <82910484+lpsorg@users.noreply.github.com>
2021-05-04 17:54:59 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "Welcome to XVIDEO" in the browser's dev tools console
-console.log("Welcome to XVIDEO");
